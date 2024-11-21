db4bbe32f868acfce393051ecdc1c053e18cc740
Janeer barrera <barrera.janeer@gmail.com>
2024-11-21 11:37:10 -0500

    Context

--- Data/PropertyManagerContext.cs
+++ Data/PropertyManagerContext.cs
@@ -8,6 +8,59 @@ using System.Threading.Tasks;
 
 namespace PropertyManagerContext.Models
 {
+
+/**/
+using Microsoft.AspNetCore.Identity;
+using PropertyManager.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.Run();
+
+/**/
+
+    var builder = WebApplication.CreateBuilder(args);
+
+// Agregar contexto de base de datos
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = true;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseAuthentication();  // Activar autenticación
+app.UseAuthorization();   // Activar autorización
+
+app.MapRazorPages();
+app.Run();
+
+
     internal class PropertyManagerContext:DbContext
     {
         public DbSet<Models.Propiedad> Propiedades { get; set; }
